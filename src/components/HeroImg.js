28f7ce2933be2d8d622eb0f2d2cac0235c8c3b7c
Abhijeet <108912315+lets-do-code@users.noreply.github.com>
2023-05-15 16:27:05 +0530

    Add someyhing

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,7 +1,9 @@
 import "./HeroImgStyle.css";
+import { Typewriter } from 'react-simple-typewriter'
 import homeImg from "../assets/fullSize.jpg"
 import React from 'react'
 import { Link } from "react-router-dom";
+
 
 const HeroImg = () => {
   return (
@@ -13,7 +15,18 @@ const HeroImg = () => {
 
         <div className="content">
         <p>I'M A FREELANCER</p>
-        <h1>React Developer</h1>
+        <h1 style={{color:" #f8d912", whiteSpace:"nowrap"}}>
+        <Typewriter
+            words={['React Developer', 'Web Developer','FullStack Developer','Software Developer']}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            
+          />
+        </h1>
         
         <div className="button">
           <Link to="/project" className="btn">Project</Link>
