8828c57d3b0450438c13568614417994bb955066
Abhijeet <108912315+lets-do-code@users.noreply.github.com>
2023-05-16 00:37:47 +0530

    Add something

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -2,6 +2,7 @@ import Proj1 from "../assets/Student-Management-System.jpg"
 import Proj2 from "../assets/speech-emotion-recognition.jpg"
 import Proj3 from "../assets/Portfolio.jpg"
 import Proj4 from "../assets/diaster.jpg"
+import Proj5 from "../assets/CounterApp.png"
 
 const ProjectCardData = [
 
@@ -14,28 +15,37 @@ const ProjectCardData = [
     },
 
     {
-        projImg:Proj2 ,
-        projTitle:"Speech Emotion Recognition",
-        projDetail:"SER is the act of attempting to recognize human emotion & affective states from speech. Used python libraries librosa, Keras, Pandas, Numpy & CNN in Machine Learning. Achieve 64% accuracy from this model on RAVDASS dataset",
+        projImg:Proj5 ,
+        projTitle:"CounterApp",
+        projDetail:"This project is a simple React Native app that allows the user to increase or decrease a number by one using two buttons.",
         view:"https://github.com/lets-do-code/speech-emotion-recognition-software-",
         source:"https://github.com/lets-do-code/speech-emotion-recognition-software-"
     },
 
     {
         projImg:Proj3 ,
-        projTitle:"My Protfolio",
-        projDetail:"This is My Portofolio website. In this wbsite i used React.js & CSS for making user Interface.",
+        projTitle:"My Portfolio",
+        projDetail:"This is My Portfolio website. In this website i used React.js & CSS for making user Interface.",
+        view:"https://singhabhijeet07.netlify.com",
+        source:"https://github.com/lets-do-code/MyPortfolio"
+    },
+    
+    {
+        projImg:Proj4 ,
+        projTitle:"Diaster Monitoring (UpComing...)",
+        projDetail:"This app provides real-time updates and alerts on potential disasters and their impact on affected areas.The app uses an interactive map to display the location of available resources, such as food and water, for those affected by the disaster.",
         view:"https://github.com/lets-do-code/speech-emotion-recognition-software-",
         source:"https://github.com/lets-do-code/speech-emotion-recognition-software-"
     },
     
     {
-        projImg:Proj4 ,
-        projTitle:"Diaster Monitoring (UpComming...)",
-        projDetail:"This app provides real-time updates and alerts on potential disasters and their impact on affected areas.The app uses an interactive map to display the location of available resources, such as food and water, for those affected by the disaster.",
+        projImg:Proj2 ,
+        projTitle:"Speech Emotion Recognition",
+        projDetail:"SER is the act of attempting to recognize human emotion & affective states from speech. Used python libraries librosa, Keras, Pandas, Numpy & CNN in Machine Learning. Achieve 64% accuracy from this model on RAVDASS dataset",
         view:"https://github.com/lets-do-code/speech-emotion-recognition-software-",
         source:"https://github.com/lets-do-code/speech-emotion-recognition-software-"
     }
+    
 ];
 
 export default ProjectCardData;
